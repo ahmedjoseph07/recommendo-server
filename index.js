a36d07e423412b8f67626a0fc0ad5264a498632e
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-06-10 20:39:12 +0600

    add-query api(create query)

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 app.get("/", (req, res) => {
-    res.send({ message: "Working" });
+    res.send({ message: "Welcome to Recommendo Server" });
 });
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -17,18 +21,36 @@ const client = new MongoClient(process.env.MONGODB_URI, {
     },
 });
 
+let database;
+let queriesCollection;
+
 async function run() {
     try {
         await client.db("admin").command({ ping: 1 });
         console.log(
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
+        database = await client.db("recommendo");
+        queriesCollection = database.collection("queries");
     } catch (err) {
-        console.log(err);
+        console.log("Database Error : ", err);
     }
 }
 run().catch(console.dir);
 
+app.post("/api/queries", async (req, res) => {
+    try {
+        const { queryData } = req.body;
+        const result = await queriesCollection.insertOne(queryData);
+        if(!queryData){
+            res.status(400).send({message:"Bad Request, No queryData found"})
+        }
+        res.status(201).send({message: "Query Inserted",insertedId: result.insertedId,});
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server Running on port ${port}`);
